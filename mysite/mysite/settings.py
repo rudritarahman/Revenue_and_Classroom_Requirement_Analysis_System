e84f4a33113afb546b7986484d33b2a312f8bfec
rudritarahman <rudritarahman@gmail.com>
2022-04-25 00:38:25 +0600

    Edit C:\Users\Asus\Desktop\RCRAS\mysite\mysite\settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -250,4 +250,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
->>>>>>> 9b58f8b (Update admin.py)
+#>>>>>>> 9b58f8b (Update admin.py)
